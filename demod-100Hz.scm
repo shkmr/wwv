da21b800cdbe19ae611a303f78262efe4ca4534d
Shigenobu Kimura <skimu@me.com>
2016-11-13 18:21:49 -0600

    decode

--- demod-100Hz.scm
+++ demod-100Hz.scm
@@ -6,8 +6,6 @@
 (use gauche.uvector)
 (use gauche.vport)
 
-(define-constant BUFSIZ 20)
-
 ;; Sample period
 (define T100Hz #f)
 (define T100ms #f)
@@ -30,7 +28,7 @@
 (define Ts  0)  ; Time index between samples.
 (define Tm  #f) ; TIme index between markers.  #f means no marker found yet.
 
-;; Output. Updated each samples
+;; Output. Updated each samples.
 (define I   0)  ; Filtered I
 (define Q   0)  ; Filtered Q
 (define A   0)  ; Analog out = sqrt(I^2 + Q^2)
@@ -41,9 +39,8 @@
 (define c1  0)  ; 1 correlatoin
 (define c0  0)  ; 2 correlation
 
-;;;
-;;;
-;;;
+;;
+;;
 (define (Ibase T)  (cos (/ (* 2 pi T) T100Hz)))
 (define (Qbase T)  (sin (/ (* 2 pi T) T100Hz)))
 
@@ -56,7 +53,7 @@
         (else     -1)))
 
 ;;
-(define Aq (make-vector 10 1))
+(define Aq (make-vector 10 1))  ; init with 1 to avoid false MM.
 
 (define (push-A A)
   (vector-copy! Aq 0 Aq 1)
@@ -110,6 +107,36 @@
            (error "Something went wrong")))))
 
 ;;
+;;
+(define S   0)   ;  Second running index
+(define S0  #f)  ;  Points zero second.
+(define codes (make-vector 60 #f))
+
+(define (inc!-S)
+  (inc! S)
+  (if (= S 60) (set! S 0)))
+
+(define (get-codes)
+  (if (not S0)
+    (make-list 60 #f)
+    (let lp ((s S0)
+             (n 60)
+             (r '()))
+      (cond ((= n 0)   (reverse r))
+            ((= s 60)  (lp 0 n r))
+            (else
+             (lp (+ s 1)
+                 (- n 1)
+                 (cons (vector-ref codes s) r)))))))
+
+(define (disp-codes)
+  (display "## codes: ")
+  (let ((l (get-codes)))
+    (print l)
+    (if (or (eq? (car l) 'MM1)
+            (eq? (car l) 'MM2))
+      (decode-wwv l))))
+
 (define (detect-code)
   (let ((M (cond ((and (> cM 7) (not Tm))
                   (print #`"## detect: Marker found, setting Tm=0, cM: ,|cM|")
@@ -124,24 +151,105 @@
                   (print #`"## detect: Out of sync marker, setting Tm=0, cM: ,|cM|")
                   (set! Tm 0)  'M0)
                  ((> cM 7)    'MF) ; false marker
-                 ((> cMM 7)   'MM)
-                 ((> c1  7)     1)
-                 ((> c0  7)     0)
+                 ((> cMM 7)   'MM) ; minute marker
+                 ((> c1  7)   'B1)
+                 ((> c0  7)   'B0)
                  (else #f))))
     (if (and Tm (= (modulo Tm 10) 0))
-      (print #`"## detect: Tm: ,|Tm| code: ,|M|"))
+      (begin (print #`"## detect: Tm: ,|Tm| code: ,|M|")
+             (every-second M)))
     (if Tm (inc! Tm))))
-;;
-(define (decode-wwv)
-  ;; for now....
-  (vector-for-each-with-index
-   (lambda (i m)
-     (print #`"## codes[,|i|]: ,|m|"))
-   codes)
-  (set! cp 0))
+
+(define prev-code #f)
+
+(define (every-second m)
+  (define (MM? x) (or (eq? x 'MM0) (eq? x 'MM1) (eq? x 'MM2)))
+  (cond ((not m) #f)
+        ((and (eq? m 'MM) (eq? prev-code 'M1))
+         (if (MM? (vector-ref codes S))
+           (vector-set! codes S 'MM1)
+           (vector-set! codes S 'MM0))
+         (set! S0 S)
+         (disp-codes))
+        ((and (eq? m 'MM) (eq? prev-code 'M2))
+         (if (MM? (vector-ref codes S))
+           (vector-set! codes S 'MM2)
+           (vector-set! codes S 'MM0))
+         (set! S0 S)
+         (disp-codes))
+        ((eq? m 'MM) #f)
+        ((eq? m 'MF) #f)
+        (else
+         (vector-set! codes S m)))
+  (set! prev-code m)
+  (inc!-S))
 
 ;;
 ;;
+(define (decode-wwv l)
+
+  (define (rbcd)
+    (let* ((d0 (read-byte))
+           (d1 (read-byte))
+           (d2 (read-byte))
+           (d3 (read-byte)))
+      (if (or (= d0 255)
+              (= d1 255)
+              (= d2 255)
+              (= d3 255))
+        'x
+        (+ (* 8 d3)
+           (* 4 d2)
+           (* 2 d1)
+           (* 1 d0)))))
+
+  (define (decode)
+    (let* ((DST1  (begin (read-byte)   ; 0
+                         (read-byte)   ; 1
+                         (read-byte))) ; 2
+           (LSW   (begin (read-byte))) ; 3
+           (Y0    (begin (rbcd)))      ; 4 5 6 7
+           (M0    (begin (read-byte)   ; 8
+                         (read-byte)   ; 9
+                         (rbcd)))      ; 10 11 12 13
+           (M1    (begin (read-byte)   ; 14
+                         (rbcd)))      ; 15 16 17 18
+           (H0    (begin (read-byte)   ; 19
+                         (rbcd)))      ; 20 21 22 23
+           (H1    (begin (read-byte)   ; 24
+                         (rbcd)))      ; 25 26 27 28
+           (D0    (begin (read-byte)   ; 29
+                         (rbcd)))      ; 30 31 32 33
+           (D1    (begin (read-byte)   ; 34
+                         (rbcd)))      ; 35 36 37 38
+           (D2    (begin (read-byte)   ; 39
+                         (rbcd)))      ; 40 41 42 43
+           (DUT1  (begin (read-byte)   ; 44
+                         (read-byte)   ; 45
+                         (read-byte)   ; 46
+                         (read-byte)   ; 47
+                         (read-byte)   ; 48
+                         (read-byte)   ; 49
+                         (read-byte))) ; 50
+           (Y1    (begin (rbcd)))      ; 51 52 53 54
+           (DST2  (begin (read-byte)))); 55
+      (print #`"## decode: ,|H1|,|H0|:,|M1|,|M0| \
+                ,|D2|,|D1|,|D0|-th day of year 20,|Y1|,|Y0|.")))
+
+  (let ((p (open-input-uvector
+            (list->u8vector
+             (map (lambda (m)
+                    (case m
+                      ((B0) 0)
+                      ((B1) 1)
+                      (else 255)))
+                  l)))))
+    (with-input-from-port p decode)))
+
+;;
+;;
+(define-constant BUFSIZ 20)
+
 (define (run)
   (let ((inbuf (make-s16vector BUFSIZ 0)))
     (read-wav-header)
